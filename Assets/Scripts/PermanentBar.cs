b9890f51b9dd708f1f8ec3d6809b76fe1a24bdb3
SugaSugaRyun <rlarkfbs@gmail.com>
2024-06-05 18:44:57 +0900

    place indicate

--- Assets/Scripts/PermanentBar.cs
+++ Assets/Scripts/PermanentBar.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PermanentBar : MonoBehaviour
 {
     private static PermanentBar instance;
-
+    public string CurrentSceneName { get; private set; }
+    [SerializeField] private TextMeshProUGUI messageBox;
     private void Awake()
     {
         if (instance != null)
@@ -15,7 +17,16 @@ public class PermanentBar : MonoBehaviour
             return;
         }
         instance = this;
+        SceneManager.sceneLoaded += OnSceneLoaded;
         DontDestroyOnLoad(gameObject);
         SceneManager.LoadScene("village_main");
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // 씬 이름 저장
+        CurrentSceneName = scene.name;
+        messageBox.text = CurrentSceneName;
+        Debug.Log("Current Scene: " + CurrentSceneName);
     }
 }
